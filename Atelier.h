940ee412edc5cde3aa83e87ce422fa7fc324c360
Gavrilescu Petru-Albert <petru-albert.gavrilescu@my.fmi.unibuc.ro>
2021-05-24 16:07:39 +0300

    Prima clasa???

---
+++ Atelier.h
@@ -0,0 +1,14 @@
+//
+// Created by Not Your Problem on 24.05.2021.
+//
+
+#ifndef POO___ATELIER_AUTO_ATELIER_H
+#define POO___ATELIER_AUTO_ATELIER_H
+
+
+class Atelier {
+
+};
+
+
+#endif //POO___ATELIER_AUTO_ATELIER_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
-//
-// Created by Gavrilescu Albert on 24.05.2021.
-//
+#include <iostream>
+#include "Atelier.h"
 
+int main() {
+    /*Atelier a("nume", 5000, 10 ); // nume, cont_curent si rating
+    Client c;
+    Masina m;
+    Reperatie r("motor", 3000);
+    Mecanic mc;
+
+    c.cere_pret();
+
+    //a.atribuie_mecanic(); asta dupa ce mai dezvolt aplicatia
+
+    std::cout << "Reparatia costa" << r.pret_total();
+
+    mc.repara_masina(m); // creste contul curent al firmei
+
+    c.lasa_rating(a);// creste ratingul atelierului sau scade, vom face o medie daca vor exista mai multe
+*/
+    std::cout << "Reparatia costa";
+
+    return 0;
+}
